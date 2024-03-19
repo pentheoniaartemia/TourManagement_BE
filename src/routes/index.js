f715f2d299875d75c0782bb2eb280035d9390397
pentheoniaartemia <162681302+pentheoniaartemia@users.noreply.github.com>
2024-03-19 21:13:37 +0700

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
 const UserRouter = require('./UserRouter');
 const TourRouter = require('./TourRouter');
 const BookingRouter = require('./BookingRouter');
+const PaymentRouter = require('./PaymentRouter');
 
 const routes = (app) => {
     app.use('/api/user', UserRouter);
     app.use('/api/tour', TourRouter);
     app.use('/api/booking',BookingRouter);
+    app.use('/api/payment',PaymentRouter);
 };
 
 module.exports = routes;
